a0cfad1983b7ac2f3f78d7d5f900590c0c545a4b
India <tanmoy587@gmail.com>
2024-03-21 19:34:20 +0530

    For moving the project

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 
-import { selectLoggedInUser, createUserAsync } from '../authSlice';
+// import { selectLoggedInUser, createUserAsync } from '../authSlice';
 import { Link } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
